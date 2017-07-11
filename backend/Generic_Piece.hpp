fb21c798cb2c5ea367eda4ef74d6029bfee3edf3
noay2 <ndjengue@uci.edu>
2017-07-10 20:36:33 -0700

    Update Generic_Piece.hpp

--- backend/Generic_Piece.hpp
+++ backend/Generic_Piece.hpp
@@ -3,6 +3,7 @@
 #define Piece_hpp
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <vector>
 #include <utility>
@@ -15,7 +16,8 @@ class Piece
     
     
     
-    Piece(std::string color, std::string type, int number, int row, int col)
+    Piece(std::string color, std::string type, int number, int row, int col, std::string * turn, std::vector<std::vector<Piece*>> * board,
+          std::vector<Piece> * Pieces)
     {
         this -> color      = color;
         this -> type       = type;
@@ -26,6 +28,12 @@ class Piece
         
         this -> just_infear        = false;
         this -> infear_and_trapped = false;
+        
+        this -> turn  = turn;
+        this -> board = board;
+        this -> rows  = board->size();
+        this -> cols  = this->board->front().size();
+        this -> Pieces= Pieces;
         
         
     }
@@ -65,7 +73,95 @@ class Piece
     
     
     
+    std::vector<std::pair<int,int>> adjacent_squares(int & col, int & row)
+    {
+        std::vector<std::pair<int,int>> adjacent_squares = std::vector<std::pair<int,int>> ();
+        for (int rowt = row-1; rowt != row +2; ++rowt)
+        {
+            for (int colt = col-1; colt!= col+2; ++colt)
+            {
+                if (rowt == row && colt == col)
+                {
+                    
+                }
+                else if( (rowt >=0) && (rowt < this->rows) &&           //Piece in bounds
+                        (colt >=0) && (colt < this->cols)
+                        )
+                {
+                    adjacent_squares.push_back({rowt, colt});
+                }
+            }
+        }
+        return adjacent_squares;
+    }
     
+    
+    bool infear(int& col, int & row)
+    {
+        
+        std::vector<std::pair<int,int>> adjacent_squares = this->adjacent_squares(col, row);
+        for(auto i = adjacent_squares.begin(); i!= adjacent_squares.end(); ++i)
+        {
+            Piece * adjacent_piece = (*board)[i->first][i->second];
+            
+            
+            if( adjacent_piece != nullptr && this->scared_of(adjacent_piece) )
+            {return true;}
+        }
+        return false;
+    }
+    
+    
+    void validmoves(std::vector<std::pair<int, int>> & validmoves)
+    {
+
+        
+        if ( (!this->just_infear)   )                                         //If Piece not afraid, but exists afraid piece, that is priority
+        {
+            for (auto i = this->Pieces->begin(); i!= this->Pieces->end(); ++i)
+            {
+                
+                if ( i->just_infear && (i->color == this->color) )
+                {return;}
+            }
+        }
+        int rowt = this->row;
+        int colt = this->col;
+        
+        for (int rowd = -1; rowd != 2; rowd++)                               //All 8 directions
+        {
+            for (int cold = -1; cold != 2; cold++)
+            {
+                if (!( rowd == 0 && cold == 0))
+                {
+                    if ( (abs(cold) == abs(rowd) && (this->type == "LION" || this->type=="ELEPHANT")) ||\
+                        ((cold ==0 || rowd ==0 ) && (this->type == "MOUSE"|| this->type=="ELEPHANT"))
+                        )                                                     //Piece can go direction
+                    {
+                        //   std::cout<<rowd<<cold<<std::endl;
+                        colt +=cold;
+                        rowt += rowd;
+                        
+                        while( (rowt >=0) && (rowt < this->rows) &&           //Piece in bounds
+                              (colt >=0) && (colt < this->cols) &&
+                              this->board->at(rowt)[colt] == nullptr            //Piece not colliding with other piece
+                              )
+                        {
+                            
+                            if( (!this->infear(colt, rowt)) || this->infear_and_trapped) //Checks if afraid or bypasses if infear/trapped
+                            {
+                                validmoves.push_back({rowt, colt});
+                            }
+                            colt+=cold;
+                            rowt+=rowd;
+                        }
+                    }
+                }
+                colt = this->col;
+                rowt = this->row;
+            }
+        }
+    }
     
     
     
@@ -82,8 +178,11 @@ class Piece
     bool                                            just_infear                ;
     bool                                            infear_and_trapped         ;
     
-
-
+    std::string *                                   turn                       ;
+    std::vector<std::vector<Piece*>> *              board                      ;
+    std::vector<Piece> *                            Pieces                     ;
+    int                                             rows                       ;
+    int                                             cols                       ;
 
 
     
