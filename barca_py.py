205c82137d038091d0c9e0666a0ff1bc42d13e51
noay2 <ndjengue@uci.edu>
2017-07-15 14:41:58 -0700

    Update barca_py.py

--- barca_py.py
+++ barca_py.py
@@ -5,13 +5,14 @@ import math
 
 ##########################################
 class Piece:
-    def __init__(self, piece_arr):
+    def __init__(self, piece_arr, board):
         self.color   = piece_arr[0]
         self.type    = piece_arr[1]
         self.row     = piece_arr[2]
         self.col     = piece_arr[3]
         self.infear  = piece_arr[4]
         self.trapped = piece_arr[5]
+        self.board = board
 
     def __repr__(self):
         return "*" +self.color[0] +self.type[0]+"*"
@@ -30,29 +31,29 @@ class Piece:
         return piece.scares(self)
 
 
-    def adjacent_squares(self,row,col,board):
+    def adjacent_squares(self,row,col):
         for rowt in range(row -1, row +2, 1):
             for colt in range(col -1, col +2, 1):
                 if (rowt == row) and (colt == col):
                     pass
-                elif (rowt <len(board)) and (rowt>=0) and (colt<len(board[0])) and (colt>=0):
+                elif (rowt <len(self.board)) and (rowt>=0) and (colt<len(self.board[0])) and (colt>=0):
                     yield [rowt, colt]
 
-    def potential_infear(self,row,col,board):
+    def potential_infear(self,row,col):
         for square in self.adjacent_squares(row,col):
-            piece = board[square[0]][square[1]]
+            piece = self.board[square[0]][square[1]]
             if (piece !=None) and (self.scared_of(piece)):
                 return True
         return False
 
     
-    def validmoves(self, board):
+    def validmoves(self):
         if not(self.infear):
             for piece in self.Pieces:
                 if piece.infear and (not piece.trapped) and (piece.color ==self.color):
                     return
-        rowt = len(board)
-        colt = len(board[0])
+        rowt = len(self.board)
+        colt = len(self.board[0])
         for rowd in range(-1,2,1):
             for cold in range(-1,2,1):
                 if (rowd ==0) and (cold==0):
@@ -61,9 +62,9 @@ class Piece:
                    or (( (cold ==0) or (rowd==0)) and (self.type == "MOUSE" or self.type == "ELEPHANT")):
                     colt += cold
                     rowt += rowd
-                    while (rowt >=0) and (rowt < len(board)) and\
-                          (colt >=0) and    (colt<len(board[0])):
-                        if (self.potential_infear(rowt,colt,board)) or self.trapped:
+                    while (rowt >=0) and (rowt < len(self.board)) and\
+                          (colt >=0) and    (colt<len(self.board[0])):
+                        if (not self.potential_infear(rowt,colt)) or self.trapped:
                             yield [rowt,colt]
                         rowt+=rowd
                         colt+=cold
@@ -71,7 +72,7 @@ class Piece:
                 colt = len(board[0])
 ##########################################              
 class Board:
-     def __init__(self,whitetomove,humantomove, Pieces,source,dest):
+     def __init__(self,whitetomove,humantomove,victory, Pieces):
         self.colors = ["BLACK", "WHITE"]
         self.types  = ["ELEPHANT", "MOUSE", "LION"]
         self.rows = 10                                  
@@ -90,24 +91,26 @@ class Board:
                                   [self.rows/2 +1, self.cols/2 -2],[self.rows/2 +1, self.cols/2 +1]
                                 
                 ]
+
+
+
+
         
         self.whitetomove = whitetomove
         self.humantomove = humantomove
-        self.Pieces = Pieces
+        self.victory = victory
         self.board = [[None for j in range(10)] for i in range(10) ]
+        self.Pieces = [ Piece(piece) for piece in Pieces]
         for piece in self.Pieces:
-            self.board[piece[2]][piece[3]]  = Piece(piece)
-        self.piece = self.board[source][dest]
+            self.board[piece.row][piece.col]  = piece
+
+
+
         
     def all_valid_moves(self):
         for piece in in self.Pieces:
             for validmoves in piece.validmoves():
                 yield [piece.row, piece.col, validmoves[0], validmoves[1]]
-
-    def update(self, source, dest):
-        
-        
-        
 
     def check_score(self):
         pass
@@ -127,10 +130,24 @@ class Board:
             return "BLACK"
         else:
             return None
+        
+    def switch_turn(self):
+        if not self.victory:
+            if self.turn =="BLACK":
+                self.turn == "WHITE"
+            else:
+                self.turn == "BLACK"
+                
+    def update(self, source, dest):
+        self.board = 
+        
+        
+
+
 ##########################################      
 class Game(self):
-    def __init__(self,whitetomove,humantomove, Pieces,source,dest):
-        self.board = Board(whitetomove,humantomove, Pieces,source,dest)
+    def __init__(self,whitetomove,humantomove, victory,Pieces,source,dest):
+        self.board = Board(whitetomove,humantomove, victory, Pieces,source,dest)
         self.human_source      = source
         self.human_dest        = dest
 ##########################################
@@ -156,7 +173,7 @@ class Backend:
                 ]
 
 
-    def send_new_data(self, whitetomove = True, humantomove = True, source = None, dest = None):
+    def send_new_data(self, whitetomove = True, humantomove = True,victory = False, source = None, dest = None):
         Pieces = []
         counter = 0
         for color in self.colors:
@@ -166,11 +183,11 @@ class Backend:
                         int(self.init_piece_position[counter][0]), int(self.init_piece_position[counter][1]),
                                   False, False])
                     counter +=1
-        return [whitetomove, humantomove, Pieces, source, dest]
+        return [whitetomove, humantomove, victory, Pieces, source, dest]
     
 
     def receive_data(self, whitetomove,humantomove, Pieces,source,dest):
-        self.game = Game(whitetomove,humantomove, Pieces,source,dest)
+        self.game = Game(whitetomove,humantomove, victory, Pieces,source,dest)
 
 
 
@@ -180,7 +197,7 @@ class Backend:
             for piece in row:
                 if piece != None:
                     Pieces.append([piece.color, piece.type, piece.row, piece.col, piece.scared, piece.trapped])
-        return [self.whitetomove, self.humantomove, Pieces, self.source, self.dest]
+        return [self.game.board.whitetomove, self.game.board.humantomove,self.game.board.victory, Pieces, self.game.source, self.game.dest]
 
 
 ##########################################
