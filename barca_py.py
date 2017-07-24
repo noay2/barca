7126437f9c2111d1f5fb51f00060e05e9c25839c
noay2 <ndjengue@uci.edu>
2017-07-24 02:07:31 -0700

    Update barca_py.py

--- barca_py.py
+++ barca_py.py
@@ -196,8 +196,9 @@ class Board:
                     black_counter +=1
                 else:
                     white_counter +=1
-        holes = white_counter if self.whitetomove else black_counter
+        holes,bad_holes = white_counter,black_counter if self.whitetomove else black_counter,white_counter
         score += [0,20,50,1000000,1000000][holes]
+        score -=[0,20,50,1000000,1000000][bad_holes]
 
         for piece in self.current_pieces():
             #Are you next to a watering hole:
