07d654bc8d9af5f83fe475ca2763114934ef4266
noay2 <ndjengue@uci.edu>
2017-08-06 10:36:47 -0700

    Update barca_py.py

--- barca_py.py
+++ barca_py.py
@@ -84,57 +84,59 @@ class Piece:
                 for piece in piece_type:
                     if piece.infear and (not piece.trapped) and (piece.color ==self.color):
                         return
-        rowt = self.row
-        colt = self.col
-        for rowd in range(-1,2,1):
-            for cold in range(-1,2,1):
-                if (rowd ==0) and (cold==0):
-                    pass
-                elif ( (abs(cold) == abs(rowd)) and (self.type == "LION" or self.type == "ELEPHANT")) \
-                   or (( (cold ==0) or (rowd==0)) and (self.type == "MOUSE" or self.type == "ELEPHANT")):
-                    colt += cold
-                    rowt += rowd
-                    while (rowt >=0) and (rowt < Piece.rows) and\
-                          (colt >=0) and    (colt<Piece.cols) and self.board_coord[rowt][colt] == None:
-                        if (not self.potential_infear_of(rowt,colt)) or self.trapped:
-                            yield [self.row, self.col, rowt , colt]
-                        rowt+=rowd
-                        colt+=cold
-                rowt = self.row
-                colt = self.col
+        else:
+            rowt = self.row
+            colt = self.col
+            for rowd in range(-1,2,1):
+                for cold in range(-1,2,1):
+                    if (rowd ==0) and (cold==0):
+                        pass
+                    elif ( (abs(cold) == abs(rowd)) and (self.type == "LION" or self.type == "ELEPHANT")) \
+                       or (( (cold ==0) or (rowd==0)) and (self.type == "MOUSE" or self.type == "ELEPHANT")):
+                        colt += cold
+                        rowt += rowd
+                        while (rowt >=0) and (rowt < Piece.rows) and\
+                              (colt >=0) and    (colt<Piece.cols) and self.board_coord[rowt][colt] == None:
+                            if (not self.potential_infear_of(rowt,colt)) or self.trapped:
+                                yield [self.row, self.col, rowt , colt]
+                            rowt+=rowd
+                            colt+=cold
+                    rowt = self.row
+                    colt = self.col
 
     def modify_fear(self, infear = None, trapped = None):
         if infear != None and trapped != None:
             self.infear = infear
             self.trapped = trapped
             return
-        if (not self.potential_infear_of(self.row, self.col)):
+        elif (not self.potential_infear_of(self.row, self.col)):
             self.infear = False
             self.trapped = False
             return
-        rowt = self.row
-        colt = self.col
-        for rowd in range(-1,2,1):
-            for cold in range(-1,2,1):
-                if (rowd ==0) and (cold==0):
-                    pass
-                elif ( (abs(cold) == abs(rowd)) and (self.type == "LION" or self.type == "ELEPHANT")) \
-                   or (( (cold ==0) or (rowd==0)) and (self.type == "MOUSE" or self.type == "ELEPHANT")):
-                    colt += cold
-                    rowt += rowd
-                    while (rowt >=0) and (rowt < Piece.rows) and\
-                          (colt >=0) and    (colt<Piece.cols) and self.board_coord[rowt][colt] == None:
-                        if (not self.potential_infear_of(rowt,colt)):
-                            self.infear = True
-                            self.trapped = False
-                            return
-                        rowt+=rowd
-                        colt+=cold
-                rowt = self.row
-                colt = self.col
-        self.infear = True
-        self.trapped= True
-        return
+        else:
+            rowt = self.row
+            colt = self.col
+            for rowd in range(-1,2,1):
+                for cold in range(-1,2,1):
+                    if (rowd ==0) and (cold==0):
+                        pass
+                    elif ( (abs(cold) == abs(rowd)) and (self.type == "LION" or self.type == "ELEPHANT")) \
+                       or (( (cold ==0) or (rowd==0)) and (self.type == "MOUSE" or self.type == "ELEPHANT")):
+                        colt += cold
+                        rowt += rowd
+                        while (rowt >=0) and (rowt < Piece.rows) and\
+                              (colt >=0) and    (colt<Piece.cols) and self.board_coord[rowt][colt] == None:
+                            if (not self.potential_infear_of(rowt,colt)):
+                                self.infear = True
+                                self.trapped = False
+                                return
+                            rowt+=rowd
+                            colt+=cold
+                    rowt = self.row
+                    colt = self.col
+            self.infear = True
+            self.trapped= True
+            return
     
 
 
